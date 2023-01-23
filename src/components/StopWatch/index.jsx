fdda32583cd371965b15170279adcd675f554483
Margarita <m.novolokova@gmail.com>
2023-01-23 11:17:59 +0200

    Add Time

--- src/components/StopWatch/index.jsx
+++ src/components/StopWatch/index.jsx
@@ -4,13 +4,23 @@ class StopWatch extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
+      //   count: 0,
+      time: new Date(0, 0, 0, 0, 0, 0, 0),
     };
     // this.start();-not!!!
     this.intervalId = null;
   }
   tisk = () => {
-    this.setState({ count: this.state.count + 1 });
+    const { time } = this.state;
+    const newTime = new Date(time);
+    newTime.setSeconds(newTime.getSeconds() + 1);
+    this.setState({ time: newTime });
+    // this.setState((state, props) => { //- примусово впершу чургу, не завжди використовувати
+    //     const { time } = this.state;
+    //     const newTime = new Date(time);
+    //     newTime.setSeconds(newTime.getSeconds() + 1);
+    //   return { time: newTime };
+    // });
   };
   start = () => {
     if (this.intervalId === null) {
@@ -23,23 +33,25 @@ class StopWatch extends Component {
   };
   reset = () => {
     this.stop();
-    this.setState({ count: 0 });
+    this.setState({ time: new Date(0, 0, 0, 0, 0, 0, 0) });
   };
   componentDidMount() {
     // для побочних ефектів
-    // this.start(); 
+    // this.start();
   }
-  componentDidUpdate() {}// життя нашого компоненту
+  componentDidUpdate() {} // життя нашого
   componentWillUnmount() {
     //для очистки побочних ефектів
     this.stop();
   }
   render() {
-    const { count } = this.state;
+    // const { count} = this.state;
+    const { time } = this.state;
     // this.start();-not!!!
     return (
       <div>
-        <h2>count: {count}</h2>
+        {/* <h2>count: {count}</h2> */}
+        <h2>time: {time.toLocaleTimeString("en-GB")}</h2>
         <button onClick={this.start}>start</button>
         <button onClick={this.stop}>stop</button>
         <button onClick={this.reset}>reset</button>
